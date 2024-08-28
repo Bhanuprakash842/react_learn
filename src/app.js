ec9acfef33319342d502ec6b098864e6fa71636a
Bhanuprakash842 <127642353+Bhanuprakash842@users.noreply.github.com>
2024-08-29 01:04:59 +0530

    07-01

--- src/app.js
+++ src/app.js
@@ -60,7 +60,10 @@ import ReactDOM from "react-dom/client"
 // );
 //JSX will  prevent cross side scripting attacks
 import Header from "./components/Header";//We can also use Header.jsx
-
+import { createBrowserRouter ,RouterProvider ,Outlet} from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 // const styleCard = {
 //   backgroundColor : "#f0f0f0",
 // };
@@ -70,10 +73,34 @@ import Body from "./components/Body";
 const AppLayout = () =>{
   return (<div className="app">
     <Header/>
-    <Body/>
+    <Outlet/>
   </div>)
 }
 
+const appRouter = createBrowserRouter([
+  {
+    path:"/",
+    element:<AppLayout/>,
+    children:[
+      {
+        path:"/",
+        element:<Body/>,
+      },
+      {
+        path:"/about",
+        element:
+        <About/>
+      },
+      {
+        path:"/contact",
+        element:<Contact/>,
+      },
+    ],
+    errorElement:<Error/>,
+  },
+  
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout/>);
+root.render(<RouterProvider router = {appRouter}/>);
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,50 @@
-import { useState } from "react";
-import {LOGO_URL} from "../utils/constants";
-const Header = () =>{
-
-  let btnName = "Login";//Local variables cannot used to change the state of Data
+import { useEffect, useState } from "react";
+import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
+const Header = () => {
+  let btnName = "Login"; //Local variables cannot used to change the state of Data
   const [btnNameReact, setbtnNameReact] = useState("Login");
-    return(
+  console.log("Header render");
+
+  //if no dependency array => useEffect is called on every render
+  //if dependency array is empty =[] => useEffect will be called on initial render(just once)
+  //if dependency array is [btNameReact] => called everytime btnNameReact is updated
+  useEffect(() => {
+    console.log("useEffect called");
+  }, [btnNameReact]);
+
+  return (
     <div className="header">
       <div className="logo">
         <img src={LOGO_URL}></img>
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>Services</li>
-          <li>About Us</li>
-          <li>Cart</li>
-          <button onClick={()=>{
-            btnNameReact === "Login"?
-            setbtnNameReact("Logout"):setbtnNameReact("Login");
-          }} className = "login">{btnNameReact}</button>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link>Cart</Link>
+          </li>
+          <button
+            onClick={() => {
+              btnNameReact === "Login"
+                ? setbtnNameReact("Logout")
+                : setbtnNameReact("Login");
+            }}
+            className="login"
+          >
+            {btnNameReact}
+          </button>
         </ul>
       </div>
     </div>
-    )
-  }
-  export default Header;
+  );
+};
+export default Header;
